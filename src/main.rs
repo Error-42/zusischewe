c2131e01d7367c4beef3ef131894b41ab1c45b40
Error-42 <erroruplus002d42@gmail.com>
2023-12-03 01:14:30 +0100

    automatically copy folder and reset

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,44 @@
 use std::{path::{Path, PathBuf}, fs::{self, File}, error::Error, ffi::OsStr};
 
 use argh::FromArgs;
+use fs_extra::dir;
 use xmltree::Element;
 
-/// ZuSi schlecht Wetter v1.0.0
+/// ZuSi schlecht Wetter v2.0.0
 /// 
 /// Modify the acceleration of all trains.
 #[derive(FromArgs, Debug)]
 struct Command {
+    #[argh(subcommand)]
+    subcommand: Subcommand,
+}
+
+#[derive(FromArgs, Debug)]
+#[argh(subcommand)]
+enum Subcommand {
+    Modify(Modify),
+    Reset(Reset),
+}
+
+/// Modify the acceleration of all trains.
+#[derive(FromArgs, Debug)]
+#[argh(subcommand, name = "modify")]
+struct Modify {
     #[argh(positional)]
     directory: PathBuf,
     #[argh(positional)]
     multiplier: f32,
+    /// do not create `_zsw` folder used for resetting
+    #[argh(switch, short = 'n')]
+    no_copy: bool,
+}
+
+/// Reset the acceleration of all trains.
+#[derive(FromArgs, Debug)]
+#[argh(subcommand, name = "reset")]
+struct Reset {
+    #[argh(positional)]
+    directory: PathBuf,
 }
 
 fn modify_file(path: &Path, multiplier: f32) -> Result<(), Box<dyn Error>> {
@@ -29,8 +56,19 @@ fn modify_file(path: &Path, multiplier: f32) -> Result<(), Box<dyn Error>> {
     Ok(())
 }
 
-fn main() {
-    let cmd: Command = argh::from_env();
+fn copy_name(dir: &PathBuf) -> PathBuf {
+    let mut to = dir.clone();
+    to.as_mut_os_string().push("_zsw");
+    to
+}
+
+fn modify(cmd: Modify) {
+    if !cmd.no_copy {
+        let to = copy_name(&cmd.directory);
+
+        dir::create(to.clone(), false).unwrap();
+        dir::copy(cmd.directory.clone(), to, &dir::CopyOptions::new().content_only(true)).unwrap();
+    }
 
     for file in fs::read_dir(cmd.directory).unwrap() {
         let path = file.unwrap().path();
@@ -40,5 +78,21 @@ fn main() {
         }
 
         modify_file(&path, cmd.multiplier).unwrap();
+    }
+}
+
+fn reset(cmd: Reset) {
+    let zsw_dir = copy_name(&cmd.directory);
+
+    dir::create(cmd.directory.clone(), true).unwrap();
+    dir::move_dir(zsw_dir, cmd.directory, &dir::CopyOptions::new().content_only(true)).unwrap();
+}
+
+fn main() {
+    let cmd: Command = argh::from_env();
+
+    match cmd.subcommand {
+        Subcommand::Modify(cmd) => modify(cmd),
+        Subcommand::Reset(cmd) => reset(cmd),
     }
 }
